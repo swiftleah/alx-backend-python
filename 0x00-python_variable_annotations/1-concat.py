a72b5a7e6ca19861436e28bf719172541c668f5f
swiftleah <leahrosehenry@gmail.com>
2024-03-14 09:03:25 +0000

    Task 1 - annotative function that returns sum of 2 strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' annotated function "concat"
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' function returns sum of 2 strings
+    '''
+    return(str1 + str2)
