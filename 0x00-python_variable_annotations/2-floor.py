c2b41e4c5eba82367d8f157cc7ce52af7a20a31c
swiftleah <leahrosehenry@gmail.com>
2024-03-14 09:08:59 +0000

    Task 2 - takes argument n (float) returns int of n

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' annotated function floor
+'''
+
+
+import math
+
+
+def floor(n: float) -> int:
+    ''' function floor takes float n and returns int of n
+    '''
+    return math.floor(n)
