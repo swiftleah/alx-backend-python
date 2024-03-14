4d7427d3793121bfa2473393f13057c55ed31704
swiftleah <leahrosehenry@gmail.com>
2024-03-14 09:00:03 +0000

    Task 0 - created type-annotated function that returns sum of 2 floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' type annotation function "add"
+'''
+
+
+def add(a: float, b: float) -> float:
+    ''' function add returns sum of 2 floats in float form
+    '''
+    return(a + b)
